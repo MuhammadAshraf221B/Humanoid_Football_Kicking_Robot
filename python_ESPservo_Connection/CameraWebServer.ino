db997ef17e531f634cc1f5bcc8efa43a1cc7dadd
Dalioy <yosoydalioy@gmail.com>
2026-05-01 00:23:01 +0300

    python_ESPservo_Connection

---
+++ python_ESPservo_Connection/CameraWebServer.ino
@@ -0,0 +1,158 @@
+// run it on arduino IDE
+#include <Arduino.h>
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ESP32Servo.h>
+#include "board_config.h"
+
+const char *ssid     = "WE_652DA4";
+const char *password = "n7j02656";
+
+const int UDP_PORT  = 4210;
+const int SERVO_PIN = 26;
+const int SERVO_MIN = 20;
+const int SERVO_MAX = 160;
+
+WiFiUDP udp;
+Servo   servoPan;
+char    packetBuf[32];
+int     currentAngle = 90;
+
+void startCameraServer();
+void setupLedFlash();
+void udpTask(void*) {
+  for (;;) {
+    int packetSize = udp.parsePacket();
+    if (packetSize > 0) {
+      int len = udp.read(packetBuf, sizeof(packetBuf) - 1);
+      packetBuf[len] = '\0';
+      int angle = constrain(atoi(packetBuf), 0, 180);
+      servoPan.write(angle);
+      currentAngle = angle;
+      Serial.printf("angle=%d\n", angle);
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_4;
+  config.ledc_timer   = LEDC_TIMER_2;
+  config.pin_d0       = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size   = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode    = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location  = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count     = 1;
+
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count     = 2;
+      config.grab_mode    = CAMERA_GRAB_LATEST;
+    } else {
+      config.frame_size  = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed 0x%x\n", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);
+    s->set_brightness(s, 1);
+    s->set_saturation(s, -2);
+  }
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
+
+#if defined(LED_GPIO_NUM)
+  setupLedFlash();
+#endif
+
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+  Serial.print("WiFi connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  startCameraServer();
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+
+  udp.begin(UDP_PORT);
+  Serial.printf("UDP listening on port %d\n", UDP_PORT);
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  servoPan.setPeriodHertz(50);
+  servoPan.attach(SERVO_PIN, 1000, 2000);
+  servoPan.write(90);
+  Serial.println("Servo ready at 90 deg");
+
+  for (int i = 0; i < 2; i++) {
+    servoPan.write(20);  delay(800);
+    servoPan.write(160); delay(800);
+    servoPan.write(90);  delay(800);
+  }
+
+  xTaskCreatePinnedToCore(udpTask, "udp_task", 4096, NULL, 2, NULL, 0);
+}
+
+void loop() {
+  delay(1000);
+}
